f09d6df2d32794260ea6a7929191d15fea9ea406
子卿 <535853415@qq.com>
2020-12-17 21:31:39 +0800

    initial

---
+++ dalsearch/sql/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS dalsearch DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE document_page (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(255) NOT NULL,
+    `title` VARCHAR(125) NOT NULL,
+    `image` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `pagecontent` TEXT NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE document_graph (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `from_page`INT(11) NOT NULL ,
+    `to_page` INT(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
